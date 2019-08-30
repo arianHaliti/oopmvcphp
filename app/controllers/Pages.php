520ca13668bb7d690aa7a1a07ba141355a393dc8
Arian Haliti <arian93haliti@gmail.com>
2019-08-30 02:37:51 +0200

    added Core.php , Controller.php

---
+++ app/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+class Pages extends Controller
+{
+
+   public function __construct()
+   { }
+   public function index()
+   {
+      $this->view("pages/index", ['title' => 'welcome']);
+   }
+   public function about()
+   {
+      $this->view("pages/about");
+   }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<?php
+require_once '../app/init.php';
+
+// Init Core Library
+
+$init = new Core;
